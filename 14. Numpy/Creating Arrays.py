4eb0d27c04c43e9716ce8fc7f8ea88529fdbf236
ashu <ashutoshmeher885@gmail.com>
2026-07-08 12:25:01 +0530

    Reorganize NumPy examples and add comments for clarity

---
+++ 14. Numpy/Creating Arrays.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+#! Creating arrays from python lists
+
+arr = np.array([1, 2, 3, 4, 5])
+
+
+
+#! Using built-in functions in numpy
+#! Zeros Array
+
+# zeros_array = np.zeros(5)     # 1D array of 0s
+# zeros_array2 = np.zeros([2, 3])     # 2D array of 0s
+# zeros_array3 = np.zeros([2, 2, 3])     # 3D array of 0s
+# print(zeros_array)
+# print(zeros_array2)
+# print(zeros_array3)
+
+
+#! Ones Array
+# ones_array = np.ones(5)
+# print(ones_array)
+
+#? these type of function is used when we want to assign some placeholder value to an array which will be updated or changed later by the user
+
+
+
+#! Constant / Full Array
+
+# constant_arr = np.full((3, 3), 15)
+# print(constant_arr)
+
+#? this function fills the matrix with the value which is given inside its parameters
+
+
+
+#! Identity Matrix
+## identity_arr = np.eye(3, 3)
+# identity_arr = np.eye(3)
+
+# print(identity_arr)
+
+#? Inserts the data diagonally
+
+
+
+#! Range Array
+range_arr = np.arange(0, 10, 2)
+print(range_arr)
+
+#? arange(start, stop, steps)
+#? works only for 1D array
+
+
+
+#! Linespace Array
