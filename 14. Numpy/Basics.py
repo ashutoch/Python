275c3f65b57333e928d4332ecea9bb7eb239c98c
ashu <ashutoshmeher885@gmail.com>
2026-07-08 11:41:51 +0530

    Update NumPy examples to include floating-point numbers and add new file for NumPy data types

--- 14. Numpy/Basics.py
+++ 14. Numpy/Basics.py
@@ -1,10 +1,17 @@
 import numpy as np
 
 #? Python list vs NumPy array
-list_data = [1, 2, 3, 4, 5]
+list_data = [1, 2, 3, 4, 5, 6.9]
 print(list_data)
 print(type(list_data))
 
-array_data = np.array([1, 2, 3, 4, 5])
+array_data = np.array([1, 2, 3, 4, 5, 6.9])
 print(array_data)
 print(type(array_data))
+# in the above arrays we mixed integers and floats together
+# NumPy always converts the entire array into a single data type for effeciency
+
+array_data2 = np.array([1, 2, 3, 4, 5, 6.9], dtype = int)
+print(array_data2)
+print(type(array_data2))
+# this forcefully converts all the data to int data type
