734793f550eb35c91054e8046dfa5d42bd0720f8
ashu <ashutoshmeher885@gmail.com>
2026-07-08 11:55:15 +0530

    Reintroduce NumPy data types example with 1D, 2D, and 3D array creation

--- 14. Numpy/Data types of NumPy.py
+++ 14. Numpy/Data types of NumPy.py
@@ -3,19 +3,19 @@ import numpy as np
 #? Basics of arrays
 # arrays are helpful when we try to store large amount of data
 
-# d1 = 123
-# d2 = 4555
-# d3 = 5678
-# arr = [123, 4555, 5678]
+d1 = 123
+d2 = 4555
+d3 = 5678
+arr = [123, 4555, 5678]
 
 #! NumPy Arrays
 #? Creating a 1D array from a list
 
-# array_1D = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
-# print(array_1D)
-# print(type(array_1D))
+array_1D = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+print(array_1D)
+print(type(array_1D))
 
-# print(array_1D.shape)
+print(array_1D.shape)
 # prints only (10,) here because the differentiating thing here is the number of columns as the row is basically 1 only
 
 #! Shape = [no. of rows, no. of columns]
@@ -24,9 +24,9 @@ import numpy as np
 
 #? Creating a 2D array from a list of lists
 
-# array_2D = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
-# print(array_2D)
-# print(array_2D.shape)
+array_2D = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
+print(array_2D)
+print(array_2D.shape)
 
 
 #? Creating a 3D array 
