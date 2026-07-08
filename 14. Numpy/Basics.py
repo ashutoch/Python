7f36d47069b1c7694bf3e30298ac46c89a201083
ashu <ashutoshmeher885@gmail.com>
2026-07-08 11:11:27 +0530

    Add example comparing Python lists and NumPy arrays

---
+++ 14. Numpy/Basics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#? Python list vs NumPy array
+list_data = [1, 2, 3, 4, 5]
+print(list_data)
+print(type(list_data))
+
+array_data = np.array([1, 2, 3, 4, 5])
+print(array_data)
+print(type(array_data))
