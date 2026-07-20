93d1ec833c66f8833c91ca207542efeefa60d5ba
ashu <ashutoshmeher885@gmail.com>
2026-07-20 19:01:22 +0530

    Add basic and advanced indexing examples; include slicing demonstration

---
+++ 14. Numpy/Module 2/Indexing & Slicing.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#! Basic Indexing
+
+# arr = np.array([10, 20, 30, 40, 50])
+# print(arr[0])
+
+
+#! Multi-dimensional indexing
+# arr_2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(arr_2d[1, 2])
+
+#! Advanced Indexing / Boolean
+# arr = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+# print(arr[arr > 50])
+
+
+#! Slicing
+arr = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+print(arr[1 : 3])
