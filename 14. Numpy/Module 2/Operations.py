e47c3f21d3ae7a7a0cfba3c99ae4f80f24f7f8c9
ashu <ashutoshmeher885@gmail.com>
2026-07-20 20:03:04 +0530

    Add basic arithmetic operations and matrix functions examples using NumPy

---
+++ 14. Numpy/Module 2/Operations.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+#? Addition
+# a = np.array([1, 2, 3])
+# b = np.array([4, 5, 6])
+# c = a + b
+# print(c)
+
+#? Subtraction
+# d = a - b
+# print(d)
+
+#? Multiplication
+# e = a * b
+# print(e)
+
+#? Division
+# f = a / b
+# f2 = a % b
+# print(f)
+# print(f2)
+
+#? Dot Product
+# g = np.dot(a, b)
+# print(g)
+
+#? Cross Product
+# h = np.cross(a, b)
+# print(h)
+
+#? Matrix Multiplication
+# i = np.matmul(a, b)
+# print(i)
+
+#? Matrix Addition
+# j = np.add(a, b)
+# print(j)
+
+#? Matrix Subtraction
+# k = np.subtract(a, b)
+# print(k)
+
+#? Matrix Multiplication
+# l = np.multiply(a, b)
+# print(l)
+
+#? Matrix Division
+# m = np.divide(a, b)
+# print(m)
+
+#? Matrix Dot Product
+# n = np.dot(a, b)
+# print(n)
+
+#? Matrix Cross Product
+# o = np.cross(a, b)
+# print(o)
+
+#? Matrix Transpose
+# p = np.transpose(a)
+# print(p)
+
+#? Min value
+# print(np.min(a))
+
+#? Max value
+# print(np.max(a))
+
+
+#! Universal Functions (ufuncs): Square Root, Exponential
