ccad4452d57fe51a2621b86b3dc56d93bc6a149b
ashu <ashutoshmeher885@gmail.com>
2026-08-09 21:11:58 +0530

    Update array size in performance optimization example: increase range for more accurate speed comparison

--- 14. Numpy/Module 3/PerformanceOptimization.py
+++ 14. Numpy/Module 3/PerformanceOptimization.py
@@ -6,13 +6,18 @@
 import time
 import numpy as np
 
-arr = np.arange(1, 10000001)
+arr = np.arange(1, 100000001)
 
 #* Using Loop
 
-start = time.time()
-square = [x**2 for x in arr]
-end = time.time()
-print(end - start)
+start1 = time.time()
+square1 = [x**2 for x in arr]
+end1 = time.time()
+print(end1 - start1)
 
 #* Using Vectorization
+
+start2 = time.time()
+square2 = arr ** 2
+end2 = time.time()
+print(end2 - start2)
