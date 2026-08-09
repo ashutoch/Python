93aa944cdc0b64ad47e379348a313c4614455804
ashu <ashutoshmeher885@gmail.com>
2026-08-09 10:38:24 +0530

    Refactor random array examples: update comments and improve formatting for clarity

--- 14. Numpy/Module 3/Random.py
+++ 14. Numpy/Module 3/Random.py
@@ -11,6 +11,16 @@ import numpy as np
 #* 5 Random integers (1 to 9)
 # print(np.random.randint(1, 9, 5))   #? (minimum, maximum, number of data) also it never selects the max value 
 
-#* 2D Random Array
-arr = np.random.randint(1, 100, (3, 3))     #? (minimum, maximum, shape)
-print(arr)
+
+#! 2D Random Array
+# arr = np.random.randint(1, 100, (3, 3))     #? (minimum, maximum, shape)
+# print(arr)
+
+
+
+#! Unique & Sorting
+#* np.unique() and np.sort()
+
+arr = np.array([1,7, 10, 3, 4, 5, 5, 6, 2, 58, 5, 3, 2, 1])
+print(np.unique(arr))
+print(np.sort(arr))
