6b893ac93d4259c4780221e87661fd18fac1177e
ashu <ashutoshmeher885@gmail.com>
2026-08-17 21:33:15 +0530

    added matploit

--- 14. Numpy/Module 3/Project.py
+++ 14. Numpy/Module 3/Project.py
@@ -3,8 +3,13 @@
 
 import numpy as py
 from matplotlib.image import imread
+import matplotlib.pyplot as plt
+
 
 
 #* Step !: Load the image
 #? imread -> converts the image into NumPy array (we do this because we are not loading the image as an image but rather a NumPy array because we are using numpy and it is also more efficient and also so we can apply some operations on it)
+
+#? e.g.: (500, 700, 3) -> 500 rows (height), 700 columns (width), 3 channels (RGB)
+
 
