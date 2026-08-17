d56ae40aa0fbe742287f9b17d6d6a65a18bd61cb
ashu <ashutoshmeher885@gmail.com>
2026-08-17 21:30:07 +0530

    Started a project to better learn what i understood in module 3

---
+++ 14. Numpy/Module 3/Project.py
@@ -0,0 +1,10 @@
+
+#! Image to Greyscale, Threshold convertor using NumPy
+
+import numpy as py
+from matplotlib.image import imread
+
+
+#* Step !: Load the image
+#? imread -> converts the image into NumPy array (we do this because we are not loading the image as an image but rather a NumPy array because we are using numpy and it is also more efficient and also so we can apply some operations on it)
+
