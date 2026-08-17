ec005b3d81f30aa0a5e6e18840a24b4a0a0b1047
ashu <ashutoshmeher885@gmail.com>
2026-08-17 21:40:50 +0530

    tried greyscale and threshold on the test image

--- 14. Numpy/Module 3/Project.py
+++ 14. Numpy/Module 3/Project.py
@@ -11,8 +11,16 @@ import matplotlib.pyplot as plt
 #? imread -> converts the image into NumPy array (we do this because we are not loading the image as an image but rather a NumPy array because we are using numpy and it is also more efficient and also so we can apply some operations on it)
 
 #? e.g.: (500, 700, 3) -> 500 rows (height), 700 columns (width), 3 channels (RGB)
+# channel = pixels
 
 
 image = imread(r"./Test.jpg")
 print("image shape ", image.shape)
 
+#! greyscale = (0.2989 * R) + (0.587 * G) + (0.114 * B)
+#! threshold = 0 and 255
+
+
+
+#* Step 2: Extract the RGB channels
+#NumPy concept here: Indexing and slicing
